3c8cc5348b55e45a37e205b8c820ce70fc20a57e
kreeshap <kreesha.patel0831@gmail.com>
2026-01-21 19:32:41 -0500

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Optional, Tuple
 import re
 import math
 
-class ImprovedSnowDayCalculator:
+class mainapp_pyr:
     """
     Enhanced Snow Day Calculator with improved accuracy.
     Key improvements:
